749725d6f57b2eea7e086805f88ec33eb86ceb30
Jevin <therealjevin@gmail.com>
2023-01-10 10:55:59 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(names, eventName) {
+    let messages = [];
+    for(let name of names) {
+        messages.push(`Thank you, ${name}, for the wonderful ${eventName} gift!`);
+    }
+    return messages;
+}
+
+function countDown(n) {
+    while(n >= 0) {
+        console.log(n--);
+    }
+}
